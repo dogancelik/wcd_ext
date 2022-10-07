2f6b71b1491f05d4751c322f09b97de46a654550
Doğan Çelik <dogancelik93@gmail.com>
2022-10-07 03:58:53 +0300

    Read settings from environment

--- wcd_ext.nim
+++ wcd_ext.nim
@@ -1,18 +1,33 @@
 import os
 import strutils
+import logging
+import strformat
+
+var consoleLog = newConsoleLogger(useStderr = true)
+addHandler(consoleLog)
 
 const
-    PrependSlash = true
-    PrependWildcard = false
-    AppendWildcard = true
-    WildcardLast = true
     SkipChars: seq[char] = @"-+="
+
+let
+    IsDebug = getEnv("WCDEXT_DEBUG", "1").parseBool
+    PrependSlash = getEnv("WCDEXT_PREPEND_SLASH", "1").parseBool
+    PrependWildcard = getEnv("WCDEXT_PREPEND_WILDCARD", "0").parseBool
+    AppendWildcard = getEnv("WCDEXT_APPEND_WILDCARD", "1").parseBool
+    WildcardLast = getEnv("WCDEXT_WILDCARD_LAST", "1").parseBool
+
+if IsDebug:
+    debug(&"IsDebug: {IsDebug}")
+    debug(&"PrependSlash: {PrependSlash}")
+    debug(&"PrependWildcard: {PrependWildcard}")
+    debug(&"AppendWildcard: {AppendWildcard}")
+    debug(&"WildcardLast: {WildcardLast}")
 
 var
     params: seq[TaintedString] = commandLineParams()
     last: seq[TaintedString] = params[params.high].split('\\')
     joined: TaintedString = ""
-    firstChar: char = params[params.high][0]
+    firstChar: char = params[params.low][0]
 
 if firstChar in SkipChars:
     discard
@@ -31,4 +46,6 @@ else:
 
     params[params.high] = joined
 
+if IsDebug:
+    debug(&"params: {params}")
 echo params.join(" ")
